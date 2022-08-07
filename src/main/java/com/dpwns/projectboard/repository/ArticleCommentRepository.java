35d95904d1e703d9026342ade68f9ed6a3ddb27d
YeJunLim <dpwns523@naver.com>
2022-08-07 21:37:43 +0900

    #10  - repository interface 작성
    db에 접근하기 위해 작성

---
+++ src/main/java/com/dpwns/projectboard/repository/ArticleCommentRepository.java
@@ -0,0 +1,7 @@
+package com.dpwns.projectboard.repository;
+
+import com.dpwns.projectboard.domain.ArticleComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+}
