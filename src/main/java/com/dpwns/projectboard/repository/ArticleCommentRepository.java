204aa0fe5787c19fa2a1a214279e7091c1498651
YeJunLim <dpwns523@naver.com>
2022-08-08 17:41:07 +0900

    #1 - Querydsl 지원을 이용하여 api 검색 옵션 추가
    ` QuerydslPredicateExecutor`
    `QuerydslBinderCustomizer`를 이용하여 간단하게 개발

--- src/main/java/com/dpwns/projectboard/repository/ArticleCommentRepository.java
+++ src/main/java/com/dpwns/projectboard/repository/ArticleCommentRepository.java
@@ -1,9 +1,32 @@
 package com.dpwns.projectboard.repository;
 
 import com.dpwns.projectboard.domain.ArticleComment;
+import com.dpwns.projectboard.domain.QArticle;
+import com.dpwns.projectboard.domain.QArticleComment;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+// querydsl추가하니까 검색기능 작동
 @RepositoryRestResource
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+public interface ArticleCommentRepository extends
+        JpaRepository<ArticleComment, Long>,
+        QuerydslPredicateExecutor<ArticleComment>,
+        QuerydslBinderCustomizer<QArticleComment>
+{
+    @Override
+    default void customize(QuerydslBindings bindings, QArticleComment root) {
+        bindings.excludeUnlistedProperties(true);       // QuerydslPredicateExecutor에 의해 article의 모든 스펙에 대한 검색이 가능함 -> 검색이 불가능하게 변경
+        bindings.including(root.content, root.createdAt, root.createdBy);
+        // 검색필터를 하나만 - first
+//        bindings.bind(root.title).first(StringExpression::likeIgnoreCase);      // like `${v}`
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);  // like `%${v}%`
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
+
 }
