7d627a1bb483fa2fb1459878df5e6a2364d401fc
YeJunLim <dpwns523@naver.com>
2022-08-09 16:53:12 +0900

    #17 - 리팩토링: 사용 안하는 import 정리

--- src/main/java/com/dpwns/projectboard/domain/Article.java
+++ src/main/java/com/dpwns/projectboard/domain/Article.java
@@ -3,10 +3,6 @@ package com.dpwns.projectboard.domain;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
