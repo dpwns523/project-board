6bd5859d5fafa7b584dbf118738823eeadf4314b
YeJunLim <dpwns523@naver.com>
2022-08-09 11:42:10 +0900

    #16 - 게시글 뷰 기본 골격 작성
    컨트롤러, 뷰, 테스트까지 한번에 작동

--- src/main/java/com/dpwns/projectboard/controller/ArticleController.java
+++ src/main/java/com/dpwns/projectboard/controller/ArticleController.java
@@ -3,6 +3,7 @@ package com.dpwns.projectboard.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -20,6 +21,13 @@ public class ArticleController {
     public String articles(ModelMap map){
         map.addAttribute("articles", List.of());
         return "articles/index";
+    }
+
+    @GetMapping("/{article-id}")
+    public String article(@PathVariable(name = "article-id") Long articleId, ModelMap map){
+        map.addAttribute("article", "article");     // TODO: 구현할 때 실제 데이터를 넣어야함
+        map.addAttribute("articleComments", List.of());
+        return "articles/detail";
     }
 
 }
