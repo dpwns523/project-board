c061ce1efdf0e9ccda7970905a946efa15a08ef0
YeJunLim <dpwns523@naver.com>
2022-08-19 20:34:09 +0900

    #43 - 회원 계정 도메인 변경사항을 영향받는 코드에 적용
    `id` -> `userId`로 id를 변경하는 내용을
    모든 자바코드에 반영

--- src/main/java/com/dpwns/projectboard/repository/UserAccountRepository.java
+++ src/main/java/com/dpwns/projectboard/repository/UserAccountRepository.java
@@ -3,5 +3,5 @@ package com.dpwns.projectboard.repository;
 import com.dpwns.projectboard.domain.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
+public interface UserAccountRepository extends JpaRepository<UserAccount, String> {
 }
