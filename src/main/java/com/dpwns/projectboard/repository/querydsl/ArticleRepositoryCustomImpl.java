ce52ad3a410111eaf1833507cdd19ec07d36f962
YeJunLim <dpwns523@naver.com>
2022-08-19 19:59:17 +0900

    #39 - 해시태그 검색 페이지를 위한 기능 구현
    해시태그 뷰에 모든 게시글에서 나오는 해시태그를 정리해서 보여주는 기능을 위해, 해시태그만 뽑아서 unique하게 보여주는 쿼리를 querydsl로 작성함
    그 외 서비스 ,컨트롤러 관련 테스트를 작성

---
+++ src/main/java/com/dpwns/projectboard/repository/querydsl/ArticleRepositoryCustomImpl.java
@@ -0,0 +1,26 @@
+package com.dpwns.projectboard.repository.querydsl;
+
+import com.dpwns.projectboard.domain.Article;
+import com.dpwns.projectboard.domain.QArticle;
+import com.querydsl.jpa.JPQLQuery;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+
+import java.util.List;
+
+public class ArticleRepositoryCustomImpl extends QuerydslRepositorySupport implements ArticleRepositoryCustom{
+
+    public ArticleRepositoryCustomImpl() {
+        super(Article.class);
+    }
+
+    @Override
+    public List<String> findAllDistinctHashtags() {
+        QArticle article = QArticle.article;
+
+        return from(article)
+                .distinct()
+                .select(article.hashtag)
+                .where(article.hashtag.isNotNull())
+                .fetch();
+    }
+}
