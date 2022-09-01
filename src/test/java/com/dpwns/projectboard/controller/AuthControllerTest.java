94ef7994c7aa1aef690377d7ce4f895b04055fb6
YeJunLim <dpwns523@naver.com>
2022-09-01 21:53:45 +0900

    #56 - 문제가 되는 테스트 코드 발견 및 수정

--- src/test/java/com/dpwns/projectboard/controller/AuthControllerTest.java
+++ src/test/java/com/dpwns/projectboard/controller/AuthControllerTest.java
@@ -1,23 +1,31 @@
 package com.dpwns.projectboard.controller;
 
 import com.dpwns.projectboard.config.SecurityConfig;
+import com.dpwns.projectboard.config.TestSecurityConfig;
+import com.dpwns.projectboard.service.ArticleService;
+import com.dpwns.projectboard.service.PaginationService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 
 @DisplayName("View 컨트롤러 - 인증")
-@Import(SecurityConfig.class)
+@Import(TestSecurityConfig.class)
 @WebMvcTest(void.class)
 public class AuthControllerTest {
     private final MockMvc mvc;
+
+    @MockBean private ArticleService articleService;
+    @MockBean private PaginationService paginationService;
 
     public AuthControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
@@ -32,6 +40,8 @@ public class AuthControllerTest {
         mvc.perform(get("/login"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+        then(articleService).shouldHaveNoInteractions();
+        then(paginationService).shouldHaveNoInteractions();
     }
 
 }
